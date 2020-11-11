9bf56dc4f93be771ffb5ef4c2eb51f54eb8e7615
nokchax <nokchax@gmail.com>
2020-11-11 21:15:03 +0900

    project init

---
+++ src/main/java/com/nokchax/study/log/db/loggingwithdb/LoggingWithDbApplication.java
@@ -0,0 +1,13 @@
+package com.nokchax.study.log.db.loggingwithdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingWithDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingWithDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nokchax/study/log/db/loggingwithdb/LoggingWithDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nokchax.study.log.db.loggingwithdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingWithDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
